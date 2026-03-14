d9b00f81f64ebd2a700a49417a872452b7c0783f
PIUS IKEOFFIAH <piuspio12@gmail.com>
2026-03-14 18:52:58 +0000

    feat: Updated flutter_qwen to libraries just added

--- lib/flutter_qwen.dart
+++ lib/flutter_qwen.dart
@@ -1,5 +1,4 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+library;
+
+export 'src/integrity_verifier.dart';
+export 'src/model_manager.dart';
